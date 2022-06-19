151ecf1965d3c0fd7fea17d58032267db4d662da
AbdulrhmanMohammed <afarg843@gmail.com>
2022-06-19 09:46:47 +0200

    adding The project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './component/todo/todo.component';
+import { FormaddComponent } from './component/formadd/formadd.component';
+import { AddedTodoComponent } from './component/added-todo/added-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    FormaddComponent,
+    AddedTodoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/added-todo/added-todo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-added-todo',
+  templateUrl: './added-todo.component.html',
+  styleUrls: ['./added-todo.component.scss']
+})
+export class AddedTodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
